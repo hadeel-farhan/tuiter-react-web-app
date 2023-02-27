cb031d8252357d94e467fbcd281aceb572a3293c
hadeel-farhan <hadeelf2001@gmail.com>
2023-02-27 18:55:07 -0500

    almost done

--- src/App.js
+++ src/App.js
@@ -2,13 +2,25 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import Labs from "./Labs";
+import HelloWorld from "./Labs/a6/hello-world";
+import Tuiter from "./tuiter";
+import { BrowserRouter } from "react-router-dom";
+import { Routes, Route } from "react-router";
+
+
 
 function App() {
   return (
-    <div className="container">
-      <h1>Hello World!</h1>
-      <Labs />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route index
+          element={<Labs />} />
+        <Route path="/hello"
+          element={<HelloWorld />} />
+        <Route path="/tuiter"
+          element={<Tuiter />} />
+      </Routes>
+    </ BrowserRouter>
   );
 }
 export default App;
